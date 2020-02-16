7c7ff184a93218d643c0c6f1ca23ac418df6fc28
Unknown <jesper@piktiv.se>
2020-02-16 18:08:24 +0100

    Remove built files from version control

--- build/greeter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Greeter(name) {
-    return 'Hello there, ' + name + '!';
-}
-exports.Greeter = Greeter;
--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var greeter_1 = require("./greeter");
-console.log(greeter_1.Greeter('Jesper'));
