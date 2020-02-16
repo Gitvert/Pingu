7c533f1801b567bfd5534d37f0e688f4f0d4304e
Unknown <jesper@piktiv.se>
2020-02-16 17:32:10 +0100

    Ignore idea directory

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 
 app.get('/', function (req, res) {
    res.send(Greeter('Jesper'));
-})
+});
 
 // start the express server
-const server = app.listen( 8080, () => { 
+app.listen( 8080, () => {
    console.log("Example app listening at port 8080");
-} );
+});
