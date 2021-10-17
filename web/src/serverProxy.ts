3a03d14364aa3d1600b3ad2749e1cd7d0a235343
Unknown <jesper@piktiv.se>
2021-10-17 18:34:47 +0200

    Add proxy class to handle communication with server

---
+++ web/src/serverProxy.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export interface Match {
+  date: string;
+  loser: number;
+  loserScore: number;
+  winner: number;
+  winnerScore: number;
+}
+
+export interface Player {
+  id: number;
+  name: string;
+}
+
+export interface ScoreboardRow {
+  id: number;
+  name: string;
+  rating: number;
+  wins: number;
+  losses: number;
+}
+
+export class ServerProxy {
+  static async getMatches(): Promise<Match[]> {
+    return (await axios.get("http://localhost:8080/matches")).data as Match[];
+  }
+
+  static async getPlayers(): Promise<Player[]> {
+    return (await axios.get("http://localhost:8080/players")).data as Player[];
+  }
+
+  static async getScoreboard(): Promise<ScoreboardRow[]> {
+    return (await axios.get("http://localhost:8080/scoreboard")).data as ScoreboardRow[];
+  }
+}
