7fb5095e4f532dee8b9432b24751e25f52aaf531
Unknown <jesper@piktiv.se>
2021-11-03 22:49:40 +0100

    Ugly displaying of all matches

--- web/src/match-history.ts
+++ web/src/match-history.ts
@@ -1,3 +1,15 @@
-export class MatchHistory {
+import {Match, ScoreboardRow, ServerProxy} from "./serverProxy";
+import {computedFrom} from "aurelia-framework";
 
+export class MatchHistory {
+  private mMatches: Match[]= [];
+
+  public async activate(): Promise<void> {
+    this.mMatches = await ServerProxy.getMatches();
+  }
+
+  @computedFrom("mScoreBoard")
+  public get matches(): Match[] {
+    return this.mMatches;
+  }
 }
