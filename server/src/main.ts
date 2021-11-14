c3877590441cc1889628d5d07bd56db81b030cf6
Unknown <jesper@piktiv.se>
2021-11-14 11:19:45 +0100

    Add ability to run server in dev or prod mode

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,11 +7,12 @@ import {getScoreboard} from "./get-scoreboard";
 import {createMatch} from "./create-match";
 import {createPlayer} from "./create-player";
 import {DatabaseHandler} from "./database/database-handler";
+import {DynamodbHandler} from "./database/dynamodb-handler";
 
 const app = express();
 app.use(bodyParser.json({}));
 
-const sqliteHandler: DatabaseHandler = new SqliteHandler();
+const databaseHandler: DatabaseHandler = getDatabaseHandler();
 
 app.all("/*", (req, res, next) => {
    res.header("Access-Control-Allow-Origin", "*");
@@ -24,25 +25,35 @@ app.get("/", (req, res) => {
 });
 
 app.get("/players", (req, res) => {
-   getPlayers(req, res, sqliteHandler);
+   getPlayers(req, res, databaseHandler);
 });
 
 app.get("/matches", (req, res) => {
-   getMatches(req, res, sqliteHandler);
+   getMatches(req, res, databaseHandler);
 });
 
 app.get("/scoreboard", (req, res) => {
-   getScoreboard(req, res, sqliteHandler);
+   getScoreboard(req, res, databaseHandler);
 });
 
 app.post("/match", (req, res) => {
-   createMatch(req, res, sqliteHandler);
+   createMatch(req, res, databaseHandler);
 });
 
 app.post("/player", (req, res) => {
-   createPlayer(req, res, sqliteHandler);
+   createPlayer(req, res, databaseHandler);
 });
 
 app.listen( 8080, () => {
-   console.log("Example app listening at port 8080");
+   console.log("App listening at port 8080");
 });
+
+function getDatabaseHandler(): DatabaseHandler {
+   if (process.argv.slice(2)[0] == "dev") {
+      console.log("Running with sqlite")
+      return new SqliteHandler();
+   } else {
+      console.log("Running with DynamoDB")
+      return new DynamodbHandler();
+   }
+}
