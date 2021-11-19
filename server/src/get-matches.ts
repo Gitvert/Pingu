4ba57a3bda3fe7a36a7e497d6529495e391f7cef
Unknown <jesper@piktiv.se>
2021-11-19 21:39:28 +0100

    Show most recent match first in match history

--- server/src/get-matches.ts
+++ server/src/get-matches.ts
@@ -4,7 +4,9 @@ import {DatabaseHandler} from "./database/database-handler";
 
 export function getMatches(req: any, res: any, databaseHandler: DatabaseHandler) {
     databaseHandler.fetchMatches().then((records) => {
-        const responseBody: MatchResponse[] = records.map((r) => new MatchResponse(r.date, r.winner, r.loser, r.winner_score, r.loser_score));
+        const responseBody: MatchResponse[] = records
+            .map((r) => new MatchResponse(r.date, r.winner, r.loser, r.winner_score, r.loser_score))
+            .sort((r1, r2) => new Date(r2.date).getTime() - new Date(r1.date).getTime());
 
         ResponseHelper.send(res, responseBody);
     }).catch(() => {
