6fa843519b6d970a01bfa03d97fa5677f7190cf8
Jesper Evertsson <jesper@piktiv.se>
2022-10-01 11:53:33 +0200

    Implement all methods in SqliteHandler

--- kotlin-server/src/main/kotlin/com/example/kotlinserver/database/SqliteHandler.kt
+++ kotlin-server/src/main/kotlin/com/example/kotlinserver/database/SqliteHandler.kt
@@ -2,10 +2,7 @@ package com.example.kotlinserver.database
 
 import com.example.kotlinserver.models.MatchModel
 import com.example.kotlinserver.models.PlayerModel
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.ResultSet
-import java.sql.Statement
+import java.sql.*
 
 class SqliteHandler : DatabaseHandler {
     override fun fetchPlayers(): List<PlayerModel> {
@@ -26,7 +23,7 @@ class SqliteHandler : DatabaseHandler {
     override fun fetchMatches(): List<MatchModel> {
         val connection = connect()
         val statement: Statement = connection.createStatement()
-        val resultSet: ResultSet = statement.executeQuery("select date, winner, loser, winner_score, loser_score from matches")
+        val resultSet: ResultSet = statement.executeQuery("select date, winner, loser, winner_score, loser_score from matches order by date desc")
         val matches: MutableList<MatchModel> = mutableListOf()
 
         while (resultSet.next()) {
@@ -44,16 +41,42 @@ class SqliteHandler : DatabaseHandler {
         return matches
     }
 
-    override fun fetchPlayerFromId(player: Int): PlayerModel {
-        TODO("Not yet implemented")
+    override fun fetchPlayerFromId(playerId: Int): PlayerModel {
+        val connection = connect()
+        val statement: Statement = connection.createStatement()
+        val resultSet: ResultSet = statement.executeQuery("select id, name from players where id = $playerId")
+
+        resultSet.next()
+        val player = PlayerModel(resultSet.getInt("id"), resultSet.getString("name"))
+
+        connection.close()
+
+        return player
     }
 
     override fun createPlayer(name: String) {
-        TODO("Not yet implemented")
+        val connection = connect()
+        val statement: PreparedStatement = connection.prepareStatement("insert into players (name) values (?)")
+        statement.setString(1, name)
+
+        statement.executeUpdate()
+        connection.close()
     }
 
     override fun recordMatch(match: MatchModel) {
-        TODO("Not yet implemented")
+        val connection = connect()
+        val statement: PreparedStatement = connection.prepareStatement(
+            "insert into matches (date, winner, loser, winner_score, loser_score) values (?, ?, ?, ?, ?)"
+        )
+
+        statement.setString(1, match.date)
+        statement.setInt(2, match.winner)
+        statement.setInt(3, match.loser)
+        statement.setInt(4, match.winnerScore)
+        statement.setInt(5, match.loserScore)
+
+        statement.executeUpdate()
+        connection.close()
     }
 
     private fun connect(): Connection {
