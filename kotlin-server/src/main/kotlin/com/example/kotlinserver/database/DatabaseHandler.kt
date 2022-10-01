4a2c6c88e0972cba3a7d8dd27db3e5a9013786d1
Jesper Evertsson <jesper@piktiv.se>
2022-10-01 22:07:05 +0200

    Update interface signature

--- kotlin-server/src/main/kotlin/com/example/kotlinserver/database/DatabaseHandler.kt
+++ kotlin-server/src/main/kotlin/com/example/kotlinserver/database/DatabaseHandler.kt
@@ -6,7 +6,7 @@ import com.example.kotlinserver.models.PlayerModel
 interface DatabaseHandler {
     fun fetchPlayers(): List<PlayerModel>
     fun fetchMatches(): List<MatchModel>
-    fun fetchPlayerFromId(player: Int): PlayerModel
+    fun fetchPlayerFromId(playerId: Int): PlayerModel
     fun createPlayer(name: String)
     fun recordMatch(match: MatchModel)
 }
